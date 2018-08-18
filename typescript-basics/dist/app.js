a4c57d31e57c7fdbcf2af26e452ee2644201c222
elsatom <elsa.tom@perfomatix.com>
2018-08-18 19:39:08 +0530

    basic typescript files added

---
+++ typescript-basics/dist/app.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log('Hello, Hey, Hii Typescript basics');
---
+++ typescript-basics/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry : './src/app.ts',
+    output: {
+        filename: 'app.js',
+        path: __dirname + './dist'
+    },
+    resolve: {
+        extensions: ['.ts','.js']
+    }
+}
